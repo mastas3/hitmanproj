48fe67130a0b811fb1fdcef378e5e18fb76a3646
Stas Maksin <stasm@traffilog.com>
2017-06-01 20:08:17 +0300

    uploading to github

--- src/About.js
+++ src/About.js
@@ -6,6 +6,11 @@ export default class About extends Component {
     return (
       <div className="about-me-page">
         <img className="about-me-image" src={image} />
+        <div className="social-links">
+          <a className="twitter-sign" href="https://twitter.com/itayhoward"><i className="fa fa-twitter"></i></a>
+          <a className="facebook-sign" href="https://facebook.com/Itay.Hitman"><i className="fa fa-facebook-official"></i></a>
+          <a className="soundcloud-sign" href="http://soundcloud.com/itayhoward"><i className="fa fa-soundcloud"></i></a>
+        </div>
       </div>
     );
   }
